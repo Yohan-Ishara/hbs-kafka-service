4e02743de5d27c9799b8d2eabe989bc3f11ee068
Yohan Ishara <yohani@healthreconconnect.com>
2024-05-09 20:32:51 +0530

    initial commit

---
+++ src/main/java/com/hrc/hbs/hbskafkaservice/HbsKafkaServiceApplication.java
@@ -0,0 +1,13 @@
+package com.hrc.hbs.hbskafkaservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HbsKafkaServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HbsKafkaServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hrc/hbs/hbskafkaservice/HbsKafkaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hrc.hbs.hbskafkaservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HbsKafkaServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
